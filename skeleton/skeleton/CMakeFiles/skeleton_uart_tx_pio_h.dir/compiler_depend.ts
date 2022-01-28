ed0f300e300640751fe6caade00a13ab1cf5ccef
VashTheProgrammer <adriano.costanzo87@gmail.com>
2022-01-28 20:47:46 +0100

    add cmakelist on drivers directory

---
+++ skeleton/skeleton/CMakeFiles/skeleton_uart_tx_pio_h.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for skeleton_uart_tx_pio_h.
---
+++ skeleton/skeleton/generated/cmake.h
@@ -0,0 +1,10 @@
+#ifndef CMAKE_H
+#define CMAKE_H
+
+// define your version_libinterface
+#define SKELETON_VERSION "v1.0"
+
+//SCHEDULER
+#define SCHEDULER_MAX_PROCESSES 16
+
+#endif // CMAKE_H
